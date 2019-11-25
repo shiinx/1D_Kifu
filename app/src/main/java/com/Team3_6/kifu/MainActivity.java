27a5292c01a79d0a8217321405f98d9304311a22
shiinx <z.shiinx@gmail.com>
2019-11-25 17:03:12 +0800

    Initial commit

---
+++ app/src/main/java/com/Team3_6/kifu/MainActivity.java
@@ -0,0 +1,14 @@
+package com.Team3_6.kifu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
