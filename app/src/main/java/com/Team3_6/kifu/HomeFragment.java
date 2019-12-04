24e601ab31ebf43f3496c8d88a5c4990c4099f1f
gladys-chua <gladyschua98@gmail.com>
2019-12-04 13:45:38 +0800

    combine

--- app/src/main/java/com/Team3_6/kifu/HomeFragment.java
+++ app/src/main/java/com/Team3_6/kifu/HomeFragment.java
@@ -39,10 +39,7 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View rootview = inflater.inflate(R.layout.fragment_home, container, false);
-
         // Inflate the layout for this fragment
-
-         View rootview = inflater.inflate(R.layout.fragment_home, container, false);
 
         mapButton = rootview.findViewById(R.id.mapButton);
 
