b304232d7b507bbc206dbd62a15037a811026b9c
bluteck97 <55570806+bluteck97@users.noreply.github.com>
2019-12-05 20:06:52 +0800

    Commit listings and postings

--- app/src/main/java/com/Team3_6/kifu/PostFragment.java
+++ app/src/main/java/com/Team3_6/kifu/PostFragment.java
@@ -20,11 +20,11 @@ public class PostFragment extends Fragment {
     }
 
 
-    @Override
+    /**@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post, container, false);
-    }
+        //return inflater.inflate(R.layout.fragment_post, container, false);
+    }*/
 
 }
