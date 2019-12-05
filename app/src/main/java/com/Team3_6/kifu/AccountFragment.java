e0ca21f28a844daf76ee95476eee3bda34445dce
gladys-chua <gladyschua98@gmail.com>
2019-12-05 17:33:55 +0800

    Editprofile

--- app/src/main/java/com/Team3_6/kifu/AccountFragment.java
+++ app/src/main/java/com/Team3_6/kifu/AccountFragment.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -24,32 +27,51 @@ public class AccountFragment extends Fragment {
         // Required empty public constructor
     }
 
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_account, container, false);
-        Button button = (Button) view.findViewById(R.id.btn_logout);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (v.getId()) {
-                    case R.id.btn_logout:
-                        FirebaseAuth.getInstance().signOut();
-                        startLoginActivity();
-                        break;
-                }
-            }
-        });
+        setHasOptionsMenu(true);
         return view;
+    }
+
+    // to specify options menu for account fragment
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        menu.clear();
+        inflater.inflate(R.menu.account_settings, menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.btn_logout:
+                FirebaseAuth.getInstance().signOut();
+                startLoginActivity();
+                return true;
+
+            case R.id.editProfile:
+                Intent intent = new Intent(this.getContext(),editProfileActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void startLoginActivity() {
         Intent intent = new Intent(getActivity(), LoginActivity.class);
         startActivity (intent);
     }
+
+
+
+
+
 
 }
---
+++ app/src/main/java/com/Team3_6/kifu/editProfileActivity.java
@@ -0,0 +1,145 @@
+package com.Team3_6.kifu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import static com.google.android.gms.common.internal.safeparcel.SafeParcelable.NULL;
+
+public class editProfileActivity extends AppCompatActivity {
+
+    private static final String NAME_KEY = "username";
+    private static final String GENDER_KEY = "gender";
+    private static final String LOCATION_KEY = "location";
+    FirebaseFirestore db;
+    private FirebaseAuth mAuth;
+    String user_email;
+
+
+    TextView username;
+    EditText username_new;
+    TextView gender;
+    EditText gender_new;
+    TextView stay;
+    EditText stay_new;
+    Button updatebutt;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editprofile);
+
+        db = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        user_email = currentUser.getEmail();
+
+        username = findViewById(R.id.username);
+        gender = findViewById(R.id.gender);
+        stay = findViewById(R.id.staylocation);
+        username_new = findViewById(R.id.username_new);
+        gender_new = findViewById(R.id.gender_new);
+        stay_new = findViewById(R.id.staylocation_new);
+        ReadUserDeets();
+
+
+        updatebutt = findViewById(R.id.updatebutt);
+        updatebutt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UpdateData();
+            }
+        });
+
+    }
+
+
+    private void ReadUserDeets() {
+        DocumentReference user = db.collection("Users").document(user_email);
+        user.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot doc = task.getResult();
+                    if (doc.get(NAME_KEY).toString() != ""){
+                        username_new.setText(doc.get(NAME_KEY).toString());
+                    }
+                    if (doc.get(GENDER_KEY).toString() != ""){
+                        gender_new.setText(doc.get(GENDER_KEY).toString());
+                    }
+                    if (doc.get(LOCATION_KEY).toString() != "") {
+                        stay_new.setText(doc.get(LOCATION_KEY).toString());
+                    }
+                }
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                    }
+                });
+    }
+
+    private void UpdateData() {
+        DocumentReference userdeets = db.collection("Users").document(user_email);
+
+        if (username_new.getText().toString() != ""){
+            userdeets.update(NAME_KEY,username_new.getText().toString()).addOnSuccessListener(new OnSuccessListener< Void >() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Toast.makeText(editProfileActivity.this, "Updated Successfully",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+            username_new.setText("");
+        }
+        if (gender_new.getText().toString() != "") {
+            userdeets.update(GENDER_KEY, gender_new.getText().toString()).addOnSuccessListener(new OnSuccessListener< Void >() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Toast.makeText(editProfileActivity.this, "Updated Successfully",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+            gender_new.setText("");
+        }
+        if (stay_new.getText().toString() != "") {
+            userdeets.update(LOCATION_KEY, stay_new.getText().toString()).addOnSuccessListener(new OnSuccessListener< Void >() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    Toast.makeText(editProfileActivity.this, "Updated Successfully",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+            stay_new.setText("");
+        }
+
+
+        ReadUserDeets();
+    }
+
+
+}
