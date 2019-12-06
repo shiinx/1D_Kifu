843802a662e5027d838fddc32c2d90a79a70bfb5
gladys-chua <gladyschua98@gmail.com>
2019-12-06 10:15:42 +0800

    seasr5ryt

--- app/src/main/java/com/Team3_6/kifu/ItemDetailActivity.java
+++ app/src/main/java/com/Team3_6/kifu/ItemDetailActivity.java
@@ -1,5 +1,6 @@
 package com.Team3_6.kifu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -8,18 +9,39 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
 
 public class ItemDetailActivity extends AppCompatActivity {
     private ImageView iv;
     private TextView name;
 
+    FirebaseFirestore fbfs;
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+        fbfs = FirebaseFirestore.getInstance();
+        mAuth = FirebaseAuth.getInstance();
 
         iv = findViewById(R.id.image);
         name = findViewById(R.id.textView2);
+
+
+        Intent accintent = getIntent();
+
+        DocumentReference readurl = fbfs.collection("Images").document(mAuth.getCurrentUser().getEmail());
+        if (getIntent().hasExtra("Item")) {
+            String s1 = accintent.getStringExtra("Item");
+
+            Glide.with(this).asBitmap().load(s1).into(iv);
+        }
+
+
 
         if (getIntent().hasExtra("image_url") && getIntent().hasExtra("image_title")) {
             String imageUrl = getIntent().getStringExtra("image_url");
